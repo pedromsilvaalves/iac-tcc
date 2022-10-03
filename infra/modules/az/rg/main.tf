81250051f6c2199c60d1224b5423298284ae9cc6
Pedro Alves <pedromsilvaalves@gmail.com>
2022-10-03 09:01:33 -0300

    feat(infra): updating rg and win-web-app modules

--- infra/modules/az/rg/main.tf
+++ infra/modules/az/rg/main.tf
@@ -1,4 +1,11 @@
 resource "azurerm_resource_group" "az_resouce_group" {
   name     = var.resource_group_name
   location = var.location
+
+  tags = {
+    Region            = var.location
+    Team              = var.team_tag
+    Environment       = var.env
+    Creator           = var.creator
+  }
 }
